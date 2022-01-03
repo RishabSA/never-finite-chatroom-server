d937bc5ff438b7b1471705f9cb2dcd95e309b685
MrHeadshtozz <rishabsaia@gmail.com>
2022-01-03 11:17:13 -0500

    Update router.js

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("The server is up and running!");
+  res.send("The server is up and running! Waiting for connections...");
 });
 
 module.exports = router;
