cecd94472bf21b1b7e97e00bcff68df8e1ff4ca6
MrHeadshtozz <rishabsaia@gmail.com>
2022-03-29 19:49:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -286,7 +286,7 @@ io.on("connection", (socket) => {
           email: "",
           text: encrypt(`${user.user} has left the room.`),
           photoURL:
-            "https://neverfinite.com/wp-content/uploads/2022/03/NeverFiniteChatroomIcon.png",
+            "https://neverfinite.com/wp-content/uploads/2022/03/NeverFiniteChatroomIcon512.png",
           createdAtDisplay: formatted_date,
           uid,
           room,
@@ -718,7 +718,7 @@ io.on("connection", (socket) => {
               email: "",
               text: encrypt("Welcome to the room!"),
               photoURL:
-                "https://neverfinite.com/wp-content/uploads/2022/03/NeverFiniteChatroomIcon.png",
+                "https://neverfinite.com/wp-content/uploads/2022/03/NeverFiniteChatroomIcon512.png",
               createdAtDisplay: formatted_date,
               uid,
               room,
@@ -732,7 +732,7 @@ io.on("connection", (socket) => {
               email: "",
               text: encrypt(`${user.user} has joined the room!`),
               photoURL:
-                "https://neverfinite.com/wp-content/uploads/2022/03/NeverFiniteChatroomIcon.png",
+                "https://neverfinite.com/wp-content/uploads/2022/03/NeverFiniteChatroomIcon512.png",
               createdAtDisplay: formatted_date,
               uid,
               room,
