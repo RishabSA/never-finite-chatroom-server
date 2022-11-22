4fe22d703d5fff86b7563441766d4aab5e9b2cd8
MrHeadshtozz <rishabsaia@gmail.com>
2022-11-22 07:41:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -276,8 +276,6 @@ io.on("connection", (socket) => {
   let userEmailSocketScope = "";
   let userActiveRoomSocketScope = "";
 
-  console.log("connection!");
-
   socket.on("userOnline", async ({ name, photoURL, email }) => {
     try {
       const result = await findOneItemByObject(UserModel, {
