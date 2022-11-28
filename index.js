d3b020a02e94761df899c9261cac6115050a9806
MrHeadshtozz <rishabsaia@gmail.com>
2022-11-28 18:32:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -957,8 +957,6 @@ io.on("connection", (socket) => {
 
   socket.on("startTypingMessage", async ({ room, email, user }) => {
     try {
-      throw new Error("testing");
-
       if (room && email && user && decrypt(user) !== "admin") {
         addTypingUser({
           room,
