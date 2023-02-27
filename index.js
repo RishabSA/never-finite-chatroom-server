4565f1b1f46e48eea477df4119b29c444fb951f8
MrHeadshtozz <rishabsaia@gmail.com>
2023-02-26 20:53:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,18 @@ app.use(
   })
 );
 require("./startup/cors")(app);
-//app.use(restrictHeaderMiddlewareFunction);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(router);
 
 const allSockets = [];
